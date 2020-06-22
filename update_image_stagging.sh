b40bba90108535746d02bc6156d8f0c1cf1ea37c
Arief Pribadi <arief.pribadi@gmail.com>
2020-06-22 14:33:07 +0700

    nyalip

--- update_image_stagging.sh
+++ update_image_stagging.sh
@@ -1,2 +1,2 @@
-ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment-stagging container-hotel=192.168.2.113:5000/laravel_image_stagging"
-ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment-stagging container-hotel=192.168.2.113:5000/laravel_image_stagging:latest"
+ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment-stagging container-hotel-stagging=192.168.2.113:5000/laravel_image_stagging"
+ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment-stagging container-hotel-stagging=192.168.2.113:5000/laravel_image_stagging:latest"
