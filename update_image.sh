8f4b1229361236208a038c379493eccd5ec58311
Arief Pribadi <arief.pribadi@gmail.com>
2020-06-23 11:30:03 +0700

    nyalip

--- update_image.sh
+++ update_image.sh
@@ -1,4 +1,4 @@
-ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_production:latest"
-ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_production"
-ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_production:latest"
-ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_production"
+ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_master:latest"
+ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_master"
+ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_master:latest"
+ssh root@192.168.3.121 "kubectl set image deployment/apr-hotel-deployment container-hotel=192.168.2.113:5000/laravel_image_master"
