a41b526dbad42b2f6b39d972f6df0d4a6faf6bda
Arief Pribadi <arief.pribadi@gmail.com>
2020-06-30 08:35:29 +0700

    bungkus & produksi

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -3,7 +3,7 @@
 @section('content') 
     <div class="row">
       <div class="medium-6 columns">
-        <h4>[PRODUCTION ENVIRONMENT] - POC dan DEMO</h4>
+        <h4>[PRODUCTION ENVIRONMENT] - For POC dan DEMO</h4>
         <img class="thumbnail" src="images/together.jpg">
       </div>
       <div class="medium-6 large-5 columns">
