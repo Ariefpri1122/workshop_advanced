caa7a40a711ca18e7ee859f3d14f59899c62b3b5
Arief Pribadi <arief.pribadi@gmail.com>
2020-07-15 06:36:00 +0700

    nyalip

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="medium-6 columns">
         <h4>[PRODUCTION ENVIRONMENT - MASTER] -  ARIEF'S POC dan DEMO</h4>
-        <img class="thumbnail" src="images/together.jpg">
+        <img class="thumbnail" src="images/umi.jpg">
       </div>
       <div class="medium-6 large-5 columns">
         </br>
