ad76312ffee21684883e44ed7686e7febe46344a
Arief Pribadi <arief.pribadi@gmail.com>
2020-10-21 07:08:14 +0700

    nyalip

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -34,7 +34,7 @@ class ClientController extends Controller
 
     public function query_redis()
     {
-        $query = Cache::remember('redis_key',10 * 60, function () {
+        $query = Cache::remember("list_client",10 * 60, function () {
             return Client::all();
         });
 
