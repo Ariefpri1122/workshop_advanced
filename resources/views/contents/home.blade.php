c675259943af54e22d29f66e444ff768ca399baf
Arief Pribadi <arief.pribadi@gmail.com>
2021-04-12 09:39:56 +0700

    nyalip

--- resources/views/contents/home.blade.php
+++ resources/views/contents/home.blade.php
@@ -3,7 +3,7 @@
 @section('content') 
     <div class="row">
       <div class="medium-6 columns">
-        <h4>[PRODUCTION ENVIRONMENT] - READ & WRITE DATA ACCESS LAYER</h4>
+        <h4>[PRODUCTION ENVIRONMENT] - WRITE DATA ACCESS LAYER</h4>
         <img class="thumbnail" src="images/together.jpg">
       </div>
       <div class="medium-6 large-5 columns">
