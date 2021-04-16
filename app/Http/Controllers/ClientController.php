a385863837699b18340d9871abac38b4393e5711
Arief Pribadi <arief.pribadi@gmail.com>
2021-04-16 17:06:03 +0700

    nyalip

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -59,11 +59,11 @@ class ClientController extends Controller
         //$data = [];
 
         //$data['clients'] = $this->client->all();
-        $data['clients'] = $this->client2->all();
+        //$data['clients'] = $this->client2->all();
 
-        //$data['clients'] = Cache::remember('clients',10 * 60, function () {
-        //    return client::all();
-        //});
+        $data['clients'] = Cache::remember('clients',10 * 60, function () {
+            return client::all();
+        });
         
 
         //$api = new api();
