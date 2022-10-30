3b216273de6d721e3cf60ab130fba7276ead7b33
Arief Pribadi <arief.pribadi@gmail.com>
2022-10-30 20:43:44 +0700

    nyalip

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -160,7 +160,7 @@ class ClientController extends Controller
             
             $client->create($data);
 
-            Client::create($request->only(['title','name', 'last_name', 'address', 'zip_code', 'city', 'state', 'email']));
+            //Client::create($request->only(['title','name', 'last_name', 'address', 'zip_code', 'city', 'state', 'email']));
 
             /*
             $api = new api(["base_uri" => 'http://192.168.1.126:8888/']);
