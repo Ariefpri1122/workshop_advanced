02776861a3cb68608f056e0c28688cf7bf991373
Arief Pribadi <arief.pribadi@gmail.com>
2022-10-30 15:52:33 +0700

    nyalip

--- app/Providers/ProducerServiceProvider.php
+++ app/Providers/ProducerServiceProvider.php
@@ -17,7 +17,7 @@ class ProducerServiceProvider extends ServiceProvider
     {
         $conf = new Conf();
 
-        $conf->set('metadata.broker.list', env('KAFKA_BROKERS', '127.0.0.1'));
+        $conf->set('metadata.broker.list', env('KAFKA_BROKERS', '192.168.3.154'));
         
         $conf->set('compression.type', 'snappy');
         
