94776d9881cc36690622882610f4f71bfe9bd5e0
Arief Pribadi <arief.pribadi@gmail.com>
2022-10-31 08:49:40 +0700

    nyalip

--- app/Client2.php
+++ app/Client2.php
@@ -9,7 +9,8 @@ class Client2 extends Model
     //
     protected $connection = 'pgsql2';
     protected $table = 'clients';
-
+    protected $fillable = ['title','name', 'last_name', 'address', 'zip_code', 'city', 'state', 'email'];
+    
     public function reservations()
     {
         return $this->hasMany('App\Reservation');
